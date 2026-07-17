13a4bbdd7cbf36b9c1590ccee744b476c6619e50
joshjenney <121117910+joshjenney@users.noreply.github.com>
2026-07-17 14:54:45 -0500

    bugfix div by 0

--- classes/planner.php
+++ classes/planner.php
@@ -273,7 +273,7 @@ class planner {
      // DEBUG: course completions
      # echo '<pre>'; print_r($data); echo '</pre>';
 
-     $unlocked = 0; $sum = 0;
+     $unlocked = 1; $sum = 1;
      foreach($data as $course)
          foreach($course as $activity){
              if($activity == 1)
